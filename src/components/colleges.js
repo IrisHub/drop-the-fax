2c7b0001e1fde9416b1e4f75e982682fbaa46819
samjgorman <samjgorman@gmail.com>
2020-11-25 17:46:53 -0800

    test

--- src/components/colleges.js
+++ src/components/colleges.js
@@ -62,19 +62,22 @@ const Colleges = ({date,count,time}) => {
 
   return (
     <div className="College-container">
+
         {timer>12 &&
           <React.Fragment> 
-              <div className="Done"> FAX CONFIRMATION </div>
+            <div className="Done"> FAX CONFIRMATION </div>
               <div className= "Confirm"> 
               <div className="Confirm-1"> 
 
-                <div className="Confirm-text"> <img src={ clock}></img> {time} </div>
-                <div className="Confirm-text"> <img src={ cal}></img> {date} </div>
+                {/* <div className="Confirm-text"> <img src={ clock}></img> {time} </div>
+                <div className="Confirm-text"> <img src={ cal}></img> {date} </div> */}
+                <div className="Confirm-text">{time}, {date} </div>
+
               </div>
                 {/* <div className="Confirm-text"> UPDATED FAX TOTAL: {count} </div> */}
                 <div className="Confirm-2"> 
-                <div className="Confirm-text"> <img src={ status}></img> SENDING 10PM - 4AM VIA FAX.PLUS </div>
-                <div className="Confirm-text"> <img src={ mail}></img> STANFORD, BERKELEY, UCLA, USC, MIT & EVERY IVY </div>
+                <div className="Confirm-text"> SENT TO: STANFORD, BERKELEY, UCLA, USC, MIT & EVERY IVY </div>
+                <div className="Confirm-text"> STATUS: ALL FAXES DELIVERED WITHIN 48 HOURS</div>
                 </div>
 
                   <SmallImage className = "Print"
