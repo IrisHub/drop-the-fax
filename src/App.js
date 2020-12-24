86a8a54523a00b211ce09b18095869c1931ddb2e
samjgorman <samjgorman@gmail.com>
2020-12-23 18:18:14 -0800

    copy changes

--- src/App.js
+++ src/App.js
@@ -129,17 +129,17 @@ function App() {
 
         <Title text = "FAQ"></Title>
         <Highlighter text = "Q: DOES THIS REALLY SEND FAXES?"></Highlighter>
-        <Paragraph text = "A: Yes. press fax and send to 13 university presidents from Stanford, Cal, UCLA, USC, MIT and every Ivy."></Paragraph>
+        <Paragraph text = "A: Yes. press fax and send to 20 university presidents from Stanford, Cal, UCLA, USC, MIT, every Ivy & more."></Paragraph>
 
         <Highlighter text = "Q: WHY NOW?"></Highlighter>
-        <Paragraph text = "A: Bills for the next semester have just arrived.  Emails, petitions and protests were ignored. It is hard to ignore a tsunami of papers flying out of a house-sized machine that sounds like a jackhammer."></Paragraph>
+        <Paragraph text = "A: Bills for the next semester just arrived.  Emails, petitions and protests got ignored. It is hard to ignore a tsunami of papers flying out of a house-sized machine that sounds like a jackhammer."></Paragraph>
 
         {/* <Highlighter text = "Q: WHO ARE U?"></Highlighter>
         
         <Paragraph text = "A: College students from California. COVID took away 99% of the value of college, but took away 0% of the price."></Paragraph> */}
 
         <Highlighter text = "Q: *UNIVERSITY PRESIDENT UNPLUGS FAX MACHINE* "></Highlighter>
-        <Paragraph text = " A:  Faxes are received online in addition to paper, so unplug away.  It's about the collective statement we make as students coming together to send 1,000,000 faxes against tuition. "></Paragraph>
+        <Paragraph text = " A:  Faxes are received online in addition to paper, so unplug away.  It's about the statement we're making as students send 1,000,000 faxes together. "></Paragraph>
 
         <Highlighter text = "Q: WHAT ABOUT THE TREES???"></Highlighter>
         <Paragraph text = " A: We're not here to destroy the environment. We're personally donating to onetreeplanted.org to offset the papers we're printing."></Paragraph>
